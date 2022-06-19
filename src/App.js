f1ec893a2baa7235c2d7c7d2512af8ad04c231bd
rahul-arakka <arakkarahul11@gmail.com>
2022-06-19 18:22:21 +0530

    Hidden API key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import React, {Component} from 'react';
-import './App.css'
-import Navbar from './components/Navbar';
-import News from './components/News';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default class App extends Component{
- render() {
-   return(
-     <BrowserRouter>
-     <Navbar/>
+export default class App extends Component {
+  apikey = process.env.ApiKeyName; 
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<News key='gotonews' pageSize={8} category=''/>}/>
-          <Route path='/general' element={<News key='general' pageSize={8} category='general'/>}/>
-          <Route path='/sports' element={<News key='sports' pageSize={8} category='sports'/>}/>
-          <Route path='/science' element={<News key='science' pageSize={8} category='science'/>}/>
-          <Route path='/technology' element={<News key='technology' pageSize={8} category='technology'/>}/>
-          <Route path='/health' element={<News key='health' pageSize={8} category='health'/>}/>
+          <Route
+            path="/"
+            element={
+              <News apikey={this.apikey} key="gotonews" pageSize={8} category="" />
+            }
+          />
+          <Route
+            path="/general"
+            element={
+              <News
+                apikey={this.apikey}
+                key="general"
+                pageSize={8}
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="/sports"
+            element={
+              <News
+                apikey={this.apikey}
+                key="sports"
+                pageSize={8}
+                category="sports"
+              />
+            }
+          />
+          <Route
+            path="/science"
+            element={
+              <News
+                apikey={this.apikey}
+                key="science"
+                pageSize={8}
+                category="science"
+              />
+            }
+          />
+          <Route path="/technology" element={ <News apikey={this.apikey} key="technology" pageSize={8} category="technology" />}/>
+          <Route path="/health"  element={ <News  apikey={this.apikey} key="health"  pageSize={8} category="health" /> } />
         </Routes>
-     </BrowserRouter>
-   )
- }
+      </BrowserRouter>
+    );
+  }
 }
